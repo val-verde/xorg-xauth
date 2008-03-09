val-verde/xorg-xauth

commit 65a0917d4661e370b50beed8f06f5577bfeea59f

get_address_info: don't allow duplicate entries to be returned in the list OS-X's getaddrinfo() can return duplicate entries (and does for localhost with the default 10.5 /etc/hosts).  This caused some annoying crashes which this patch takes care of.
See http://trac.macosforge.org/projects/xquartz/ticket/44

gethost.c
@@ -307,7 +307,15 @@ struct addrlist *get_address_info (
 		family = FamilyInternet6;
 	    }
 
-	    if (len > 0 && src != NULL) {
+	    struct addrlist *duplicate;
+	    for(duplicate = retval; duplicate != NULL; duplicate = duplicate->next) {
+		if(duplicate->family == family && duplicate->len == len &&
+                   memcmp(duplicate->address, src, len) == 0) {
+		    break;
+                }
+	    }
+
+	    if (len > 0 && src != NULL && duplicate == NULL) {
 		struct addrlist *newrv = malloc (sizeof(struct addrlist));
 		if (newrv) {
 		    newrv->address = malloc (len);

commit 3cc17297124ea5c9917e6eacc0ab7ae021e8a386

C89 fix
65a0917d4661e370b50beed8f06f5577bfeea59f broke the build with non-C99
aware compilers.

gethost.c
@@ -295,6 +295,8 @@ struct addrlist *get_address_info (
 	hints.ai_protocol = 0;	
         if (getaddrinfo(host,NULL,&hints,&firstai) !=0) return NULL;
 	for (ai = firstai; ai != NULL; ai = ai->ai_next) {
+	    struct addrlist *duplicate;
+
 	    if (ai->ai_family == AF_INET) {
 		struct sockaddr_in *sin = (struct sockaddr_in *)ai->ai_addr;
 		src = &(sin->sin_addr);
@@ -307,7 +309,6 @@ struct addrlist *get_address_info (
 		family = FamilyInternet6;
 	    }
 
-	    struct addrlist *duplicate;
 	    for(duplicate = retval; duplicate != NULL; duplicate = duplicate->next) {
 		if(duplicate->family == family && duplicate->len == len &&
                    memcmp(duplicate->address, src, len) == 0) {